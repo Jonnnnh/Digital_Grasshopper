e4ce6bae43c889c312455b2c81f66c4961f1db6d
Daria <jonshilin.gg@gmail.com>
2024-04-02 13:21:25 +0300

    Fixed drawing logic

--- gui/MainWindow.py
+++ gui/MainWindow.py
@@ -1,9 +1,8 @@
 from PyQt5 import QtWidgets, uic
 import os
-
 from PyQt5 import QtSvg
 from PyQt5.QtGui import QMouseEvent, QPainter, QStandardItemModel
-from PyQt5.QtWidgets import QStyleOptionViewItem, QHeaderView, QMessageBox
+from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtCore import QModelIndex, QRectF, Qt
 
 from model.Game import Game
@@ -21,97 +20,76 @@ class MainWindow(QtWidgets.QMainWindow):
             uic.loadUi(ui_path, self)
         except Exception as e:
             raise RuntimeError(f"Error loading UI file: {e}")
-        uic.loadUi(ui_path, self)
 
         self.setFixedSize(720, 800)
 
         self.center_window()
-        if not hasattr(self, 'tableView'):
-            raise AttributeError(
-                "UI file does not contain 'tableView'. Check if the UI file is correct and the path is valid.")
-
+        self.check_ui_components()
+        self._images = {}
         images_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'resources/images')
-        self._images = {
-            os.path.splitext(f)[0]: QtSvg.QSvgRenderer(os.path.join(images_dir, f))
-            for f in os.listdir(images_dir)
-        }
+        self.load_image_resources(images_dir)
 
         self.game = Game(1)
-        self.game_resize(self.game)
-        self.tableView.setItemDelegate(MyDelegate(self))
-        self.pushButton.clicked.connect(self.new_game)
+        self.initialize_game_view()
+
+        self.pushButton.clicked.connect(lambda: self.start_new_game(self.spinBox.value()))
         self.pushButton_2.clicked.connect(self.close)
 
-        def new_mouse_press_event(e: QMouseEvent) -> None:
-            idx = self.tableView.indexAt(e.pos())
-            self.on_item_clicked(idx, e)
-
-        self.tableView.mousePressEvent = new_mouse_press_event
+        self.tableView.mousePressEvent = self.handle_mouse_press_event
 
     def center_window(self):
         screen = QtWidgets.QDesktopWidget().screenGeometry()
-        center_x = (screen.width() - self.width()) // 2
-        center_y = (screen.height() - self.height()) // 2
-        self.move(center_x, center_y)
+        self.move((screen.width() - self.width()) // 2, (screen.height() - self.height()) // 2)
 
-    def game_resize(self, game: Game) -> None:
-        if not isinstance(game, Game):
-            raise TypeError("Argument 'game' must be an instance of Game")
+    def check_ui_components(self):
+        if not hasattr(self, 'tableView'):
+            raise AttributeError("UI file does not contain 'tableView'.")
+
+    def load_image_resources(self, images_dir):
+        self._images = {os.path.splitext(f)[0]: QtSvg.QSvgRenderer(os.path.join(images_dir, f))
+                        for f in os.listdir(images_dir)}
+
+    def initialize_game_view(self):
+        self.resize_game_table(self.game)
+        self.tableView.setItemDelegate(MyDelegate(self))
+
+    def resize_game_table(self, game: Game):
         model = QStandardItemModel(game.row_count, game.col_count)
         self.tableView.setModel(model)
-        row_height = 114
-        column_width = 119
         for row in range(game.row_count):
-            self.tableView.setRowHeight(row, row_height)
+            self.tableView.setRowHeight(row, 114)
         for col in range(game.col_count):
-            self.tableView.setColumnWidth(col, column_width)
-        self.update_view()
+            self.tableView.setColumnWidth(col, 119)
+        self.refresh_game_view()
 
-    def new_game(self):
-        try:
-            self.game = Game(self.spinBox.value())
-        except Exception as e:
-            print(f"Error creating new game: {e}")
-        else:
-            self.update_view()
+    def start_new_game(self, level):
+        self.game = Game(level)
+        self.initialize_game_view()
 
-    def update_view(self):
+    def refresh_game_view(self):
         self.tableView.viewport().update()
 
-    def on_item_paint(self, e: QModelIndex, painter: QPainter, option: QStyleOptionViewItem) -> None:
+    def paint_game_cell(self, e: QModelIndex, painter: QPainter, option):
         cell = self.game.field[e.row()][e.column()]
-        img = self._images['default']
+        img_name = 'default'
         if cell.block:
-            name = ""
-            if cell.is_active:
-                name = name + "active "
-            if cell.is_locked:
-                name = name + "locked "
-            if cell.number == 1:
-                name = name + "1"
-            if cell.number == 2:
-                name = name + "2"
-            if cell.number == 3:
-                name = name + "3"
-            if cell.number == 4:
-                name = name + "4"
-            img = self._images[name]
-        if cell.step:
-            img = self._images['step']
+            img_name = f"{'active ' if cell.is_active else ''}{'locked ' if cell.is_locked else ''}{cell.number if cell.number > 0 else ''}".strip()
+        elif cell.step:
+            img_name = 'step'
+        img = self._images.get(img_name, self._images['default'])
         img.render(painter, QRectF(option.rect))
 
-    def on_item_clicked(self, e: QModelIndex, me: QMouseEvent = None) -> None:
-        if me.button() == Qt.LeftButton or me.button() == Qt.RightButton:
-            self.game.on_button_click(e.row(), e.column())
-            self.is_game_over()
-        self.update_view()
+    def handle_mouse_press_event(self, e: QMouseEvent):
+        if e.button() in [Qt.LeftButton, Qt.RightButton]:
+            idx = self.tableView.indexAt(e.pos())
+            self.process_cell_click(idx)
 
-    def is_game_over(self):
+    def process_cell_click(self, e: QModelIndex):
+        self.game.on_button_click(e.row(), e.column())
+        self.check_game_over()
+        self.refresh_game_view()
+
+    def check_game_over(self):
         if self.game.is_completed or not self.game.has_moves_left:
-            message = "Game Over!"
-            if self.game.is_completed:
-                message += "Congratulations, you've completed the game!"
-            else:
-                message += "No more moves left"
-
+            message = "Game Over! Congratulations, you've completed the game!" if self.game.is_completed else "Game Over! No more moves left"
             QMessageBox.information(self, "Game Over", message)
--- gui/MyDelegate.py
+++ gui/MyDelegate.py
@@ -5,9 +5,10 @@ from PyQt5.QtWidgets import QItemDelegate, QStyleOptionViewItem
 
 class MyDelegate(QItemDelegate):
     def __init__(self, parent=None, *args):
-        QItemDelegate.__init__(self, parent, *args)
+        super().__init__(parent, *args)
 
     def paint(self, painter: QPainter, option: QStyleOptionViewItem, idx: QModelIndex):
         painter.save()
-        self.parent().on_item_paint(idx, painter, option)
+        if hasattr(self.parent(), 'paint_game_cell'):
+            self.parent().paint_game_cell(idx, painter, option)
         painter.restore()
