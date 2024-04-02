07bf789f49d48b8b3de0b631008a44a9d28672e4
Daria <jonshilin.gg@gmail.com>
2024-04-02 15:08:16 +0300

    iml

--- gui/MyDelegate.py
+++ gui/MyDelegate.py
@@ -1,14 +1,14 @@
-from PyQt5.QtCore import QModelIndex
-from PyQt5.QtGui import QPainter
 from PyQt5.QtWidgets import QItemDelegate, QStyleOptionViewItem
-
+from PyQt5.QtGui import QPainter
+from PyQt5.QtCore import QModelIndex
 
 class MyDelegate(QItemDelegate):
     def __init__(self, parent=None, *args):
-        super().__init__(parent, *args)
+        super(MyDelegate, self).__init__(parent, *args)
 
     def paint(self, painter: QPainter, option: QStyleOptionViewItem, idx: QModelIndex):
         painter.save()
-        if hasattr(self.parent(), 'paint_game_cell'):
-            self.parent().paint_game_cell(idx, painter, option)
+        mainWindow = self.parent()
+        if hasattr(mainWindow, 'paint_game_cell'):
+            mainWindow.paint_game_cell(idx, painter, option)
         painter.restore()
